4658e6e2535f834b2d1eb059ffe4e095c47d5e5a
CCranney <11773171+CCranney@users.noreply.github.com>
2025-11-15 18:56:17 -0800

    mirror environment threshold switching enabled

--- main.gd
+++ main.gd
@@ -2,10 +2,45 @@ extends Node3D
 
 @onready var mirror_mesh : MeshInstance3D = $Mirror
 @onready var mirror_world_viewport : SubViewport = $MirrorWorldViewport
+@onready var current_environment: WorldEnvironment = $CurrentEnvironment
+@onready var day_environment: Environment = load("res://environments/day_environment.tres")
+@onready var night_environment: Environment = load("res://environments/night_environment.tres")
+@onready var main_camera: Camera3D = $Player/SpringArm3D/RemoteTransform3D/Camera3D
+@onready var mirror_camera: Camera3D = $MirrorWorldViewport/Camera3D
+@onready var mirror_plane: MeshInstance3D = $Mirror
+@onready var backup_mirror_plane: MeshInstance3D = $BackupMirrorForHalfwayThrough
 
 var mat : ShaderMaterial
+var sun_directional_light_mask_bit = 3
+var moon_directional_light_mask_bit = 4
+
 
 func _ready():
+	current_environment.environment = day_environment
 	mirror_world_viewport.size = get_viewport().size
 	mat = mirror_mesh.get_active_material(0) as ShaderMaterial
 	mat.set_shader_parameter("mirror_world_camera_texture", mirror_world_viewport.get_texture())
+	
+func _process(_delta):
+	if main_camera.global_position.z <= 0:
+		mirror_plane.rotation.y = PI
+		backup_mirror_plane.rotation.y = PI
+		backup_mirror_plane.position.z = 0.5
+		current_environment.environment = day_environment
+		main_camera.set_cull_mask_value(sun_directional_light_mask_bit, true) 
+		main_camera.set_cull_mask_value(moon_directional_light_mask_bit, false) 
+
+		mirror_camera.environment = night_environment
+		mirror_camera.set_cull_mask_value(sun_directional_light_mask_bit, false) 
+		mirror_camera.set_cull_mask_value(moon_directional_light_mask_bit, true) 
+	else:
+		mirror_plane.rotation.y = 0
+		backup_mirror_plane.rotation.y = 0
+		backup_mirror_plane.position.z = -0.5
+		current_environment.environment = night_environment
+		main_camera.set_cull_mask_value(sun_directional_light_mask_bit, false) 
+		main_camera.set_cull_mask_value(moon_directional_light_mask_bit, true) 
+		
+		mirror_camera.environment = day_environment
+		mirror_camera.set_cull_mask_value(sun_directional_light_mask_bit, true) 
+		mirror_camera.set_cull_mask_value(moon_directional_light_mask_bit, false) 
