f680efc12d6d404925dd4a54ae2b244c7841834f
CCranney <11773171+CCranney@users.noreply.github.com>
2025-11-22 10:58:35 -0800

    adding pause menu

--- nodes/user_interface.gd
+++ nodes/user_interface.gd
@@ -8,12 +8,14 @@ extends Control
 @onready var timer : Timer = $Timer
 @onready var finish_screen : Control = $FinishScreen
 @onready var finish_screen_label : Label = %FinishScreenLabel
+@onready var pause_screen : Control = $PauseScreen
 
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	intro_text.visible = false
 	timer_info.visible = false
 	finish_screen.visible = false
+	pause_screen.visible = false
 	timer_label.text = str(timer.time_left)
 	get_tree().paused = true	
 	process_mode = Node.PROCESS_MODE_ALWAYS
@@ -63,7 +65,7 @@ func _set_timer_and_start_game(is_timer_on: bool = true):
 func end_game_screen(is_victorious: bool) -> void:
 	get_tree().paused = true	
 	timer.paused = true
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	background_texture.visible = true
 	if is_victorious:
 		finish_screen.visible = true
@@ -75,10 +77,6 @@ func end_game_screen(is_victorious: bool) -> void:
 
 func _on_timer_timeout() -> void:
 	end_game_screen(false)
-	
-func _on_continue_button_pressed() -> void:
-	intro_text.visible = false
-	main_menu.visible = true
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().reload_current_scene()
@@ -87,5 +85,21 @@ func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+	var are_menus_open = intro_text.visible or main_menu.visible or finish_screen.visible
+	if event.is_action_pressed("ui_cancel") and not are_menus_open and not pause_screen.visible:
+		get_tree().paused = true	
+		pause_screen.visible = true
+		timer.paused = true
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	elif event.is_action_pressed("ui_cancel") and not are_menus_open and pause_screen.visible:
+		_unpause_game()
+		
+func _on_continue_button_pressed() -> void:
+	_unpause_game()
+
+func _unpause_game() -> void:
+	get_tree().paused = false	
+	pause_screen.visible = false	
+	timer.paused = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
